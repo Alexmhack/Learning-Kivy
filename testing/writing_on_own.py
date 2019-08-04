57f300412cf4a3716aa13b56aab98fde241a6330
alexHooper <alexmohack@gmail.com>
2019-08-04 19:23:41 +0530

    define input data page gridlayout

--- testing/writing_on_own.py
+++ testing/writing_on_own.py
@@ -1,7 +1,18 @@
 import kivy
+import requests
+import json
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+
+
+class DataInputPage(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+		self.cols = 1
 
 
 class TestApp(App):
