30991d14d1550a9de22ccd3bd0dc9d23d7841e55
alexHooper <alexmohack@gmail.com>
2019-08-04 19:14:40 +0530

    writing everything from scratch

---
+++ testing/writing_on_own.py
@@ -0,0 +1,13 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class TestApp(App):
+	def build(self):
+		return Label(text="Hey")
+
+
+if __name__ == '__main__':
+	TestApp().run()
