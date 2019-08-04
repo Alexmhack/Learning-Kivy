9952fc42b4423aa58db821785d5a1c0656c9041d
alexHooper <alexmohack@gmail.com>
2019-08-04 14:03:16 +0530

    start creating buttons

---
+++ kivyapp/app_v2.py
@@ -0,0 +1,34 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+
+
+class ConnectPage(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+		self.cols = 2
+
+		self.add_widget(Label(text="IP:"))
+		self.ip = TextInput(multiline=False)
+		self.add_widget(self.ip)
+
+		self.add_widget(Label(text="Username:"))
+		self.username = TextInput(multiline=False)
+		self.add_widget(self.username)
+
+		self.add_widget(Label(text="Port:"))
+		self.port = TextInput(multiline=False)
+		self.add_widget(self.port)
+
+
+class EpicApp(App):
+	def build(self):
+		return ConnectPage()
+
+
+if __name__ == '__main__':
+	EpicApp().run()
