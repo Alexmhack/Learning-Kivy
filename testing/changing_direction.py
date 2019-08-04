53eda38408191cfa31e8aa06bc780c1b65ef0292
alexHooper <alexmohack@gmail.com>
2019-08-04 18:38:35 +0530

    define builder for left and right slide transition

--- testing/changing_direction.py
+++ testing/changing_direction.py
@@ -1,5 +1,29 @@
 import kivy
 
+from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_string("""
+<MenuScreen>:
+	BoxLayout:
+		Button:
+			text: 'Goto settings'
+			on_press:
+				root.manager.transition.direction = 'left'
+				root.manager.current = 'settings'
+		Button:
+			text: 'Quit'
+
+<SettingsScreen>:
+	BoxLayout:
+		Button:
+			text: 'My settings button'
+		Button:
+			text: 'Back to menu'
+			on_press:
+				root.manager.transition.direction = 'right'
+				root.manager.current = 'menu'
+""")
