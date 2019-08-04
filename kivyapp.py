226caba24a1f91c0124ba107d110326b537712e2
alexHooper <alexmohack@gmail.com>
2019-08-04 13:30:00 +0530

    run basic kivy app

---
+++ kivyapp.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+# run only in this version of kivy
+kivy.require('1.11.1')
+
+
+# App requries only build
+class EpicApp(App):
+	def build(self):
+		return Label(text='Hey there')
+
+
+if __name__ == '__main__':
+	EpicApp().run()
