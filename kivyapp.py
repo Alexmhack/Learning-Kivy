58efa2bb24a2ff04710f138d9649b0fdc96f301c
alexHooper <alexmohack@gmail.com>
2019-08-07 21:19:54 +0530

    change acc to flake8 linter

--- kivyapp.py
+++ kivyapp.py
@@ -1,8 +1,6 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uid.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 
 # run only in this version of kivy
 kivy.require('1.11.1')
@@ -10,9 +8,9 @@ kivy.require('1.11.1')
 
 # App requries only build
 class EpicApp(App):
-	def build(self):
-		return Label(text='Hey there')
+    def build(self):
+        return Label(text='Hey there')
 
 
 if __name__ == '__main__':
-	EpicApp().run()
+    EpicApp().run()
